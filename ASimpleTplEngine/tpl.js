9296ab9ab7f4f7f1767cd18b642e147997273bd3
Ge-yuan-jun <daydaygyj@gmail.com>
2016-12-09 16:17:27 +0800

    add some code

--- ASimpleTplEngine/tpl.js
+++ ASimpleTplEngine/tpl.js
@@ -1,4 +1,20 @@
+/**
+ * <ul>
+ *  <% for ( var i = 0; i < users.length; i++ ) { %>
+ *       <li><a href="<%=users[i].url%>"><%=users[i].name%></a></li>
+ *  <% } %>
+ * </ul>
+ */
 
-const tpl = (function() {
-  // 暂时想不到怎么做好字符串的拼接工作
+const tpl = (() => {
+  let cache = {};
+  /**
+   * param:
+   * str String html结构的id名
+   * data String 模板结构
+   */
+  this.tpl = function tpl(str, data) {
+    const fn = !/\W/.test(str) ? cache[str] = cache[str] || tpl(document.getElementById(str).innerHTML) : new Function();
+    return data ?　fn(data) : fn;
+  }
 })()
