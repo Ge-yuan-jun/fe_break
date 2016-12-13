6bda62e5428a6bc924c6fd8eefcc4b919370a784
Ge-yuan-jun <daydaygyj@gmail.com>
2016-12-14 01:13:12 +0800

    add js

--- ASimpleTplEngine/tpl.js
+++ ASimpleTplEngine/tpl.js
@@ -14,7 +14,18 @@ const tpl = (() => {
    * data String 模板结构
    */
   this.tpl = function tpl(str, data) {
-    const fn = !/\W/.test(str) ? cache[str] = cache[str] || tpl(document.getElementById(str).innerHTML) : new Function();
+    const fn = !/\W/.test(str) ? cache[str] = cache[str] || tpl(document.getElementById(str).innerHTML) : new Function('obj',
+        'let p = [], print=function() {p.push.apply(p, arguments);};' +
+
+        // 利用with进行编译
+        'with(obj) {p.push(' +
+
+        str
+          .replace(/[\r\t\n]/g, ' ')  // 替换回车、制表、换行符
+          .split('<%').join('\t') // 替换 <%
+
+        ')}'
+      );
     return data ?　fn(data) : fn;
   }
 })()
