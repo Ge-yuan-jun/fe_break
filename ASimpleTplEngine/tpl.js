4f8ed424b04e0f095abc589e9e1d082f24446ec1
GeYuanjun <daydaygyj@gmail.com>
2016-12-16 17:17:07 +0800

    add function

--- ASimpleTplEngine/tpl.js
+++ ASimpleTplEngine/tpl.js
@@ -1,5 +1,5 @@
 /**
- * link: http://www.cnblogs.com/dolphinX/p/3489269.html
+ * link: http://www.cnblogs.com/hustskyking/p/principle-of-javascript-template.html
  * <ul>
  *  <% for ( var i = 0; i < users.length; i++ ) { %>
  *       <li><a href="<%=users[i].url%>"><%=users[i].name%></a></li>
@@ -13,4 +13,23 @@ let data = {
   age: '20'
 }
 
-let result = tpl.replace(/<%([^%>]+)?%>/g, (s0,s1) => data[s1])
+// let result = tpl.replace(/<%([^%>]+)?%>/g, (s0,s1) => data[s1])
+let tplEngine = (tpl, data) => {
+  const reg = /<%([^%>]+)?%>/g;
+  let [code, cursor, match] = ['let r = []; \n', 0, []];  // cursor的作用定位所匹配的代码的最后一节
+  const add = (line) => {
+    code += `r.push("${line.replace(/"/g, '\\"')}");\n`
+  }
+
+  while (match = reg.exec(tpl)) {
+    add(tpl.slice(cursor, match.index)); // 添加非逻辑部分
+    add(match[1]); // 添加逻辑部分 match[0] = "<%" + match[1] + "%>"
+    cursor = match.index + match[0].length;
+  }
+
+  add(tpl.substr(cursor, tpl.length - cursor)); // 增加代码的最后一段
+
+  code += 'return r.join("");'; // 返回结果
+
+  console.log(code)
+}
