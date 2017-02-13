dfa0855715c7772c0f7553222da0ac201260222c
GeYuanjun <daydaygyj@gmail.com>
2017-02-13 15:23:57 +0800

    add another solution

--- Algorithm/leetCode/medianOfTwoSortedArrays/index.js
+++ Algorithm/leetCode/medianOfTwoSortedArrays/index.js
@@ -43,6 +43,7 @@ var quick_sort = function(arr) {
 
 findMedianSortedArrays([1,3], [2,4])
 
+
 /**
  * 利用ES5的sort方法实现
  * @param {number[]} nums1
@@ -65,4 +66,48 @@ var findMedianSortedArrays = function(nums1, nums2) {
     }
     
     return res
+};
+
+/**
+ * 利用对比两个数组元素之间的大小直接排序
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+
+var findMedianSortedArrays = function(nums1, nums2) {
+    let [
+      n, m, i, size, resultArr
+    ] = [
+      0, 0, 0, nums1.length + nums2.length, []
+    ];
+    let medianSize = Math.floor(size / 2);
+
+    for (; i < size; i++) {
+      // 不同情况的归属
+      if (nums1[m] === undefined && nums2[n] === undefined) {
+        break;
+      } else if (nums1[m] === undefined) {
+        resultArr.push(nums2[n]);
+        n ++;
+      } else if (nums2[n] === undefined) {
+        resultArr.push(nums1[m]);
+        m ++;
+      } else if (nums1[m] < nums2[n]) {
+        resultArr.push(nums1[m]);
+        m ++;
+      } else {
+        resultArr.push(nums2[n]);
+        n ++;
+      }
+
+      // 优化点：无需排序完所有的元素即可挑出循环
+      if (resultArr.length > (medianSize + 1)) {
+        break;
+      }
+    }
+
+    return size % 2 === 1 ? 
+          resultArr[medianSize] :
+          ((resultArr[medianSize - 1] + resultArr[medianSize]) / 2);
 };
