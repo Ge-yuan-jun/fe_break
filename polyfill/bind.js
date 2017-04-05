2ccf3cb19f8ff90e6321c88852ad9354ae7f69ce
GeYuanjun <daydaygyj@gmail.com>
2017-04-05 18:13:03 +0800

    add error handle

--- polyfill/bind.js
+++ polyfill/bind.js
@@ -27,6 +27,10 @@ Function.prototye.bind = Funcction.prototype.bind || function (context) {
 
 // 构造函数下，绑定的this就需要忽略
 Function.prototype.bind = Function.prototype.bind || function (context) {
+    if (typeof this !== 'function') {
+        throw new TypeError('what is trying to bind is not a function')
+    }
+
     const me = this;
     const args = Array.prototype.slice.call(arguments, 1);
 
