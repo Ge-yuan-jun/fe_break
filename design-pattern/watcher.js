afe2fcb7e559c9dfd4f3c29513cddb0cfb30bf26
GeYuanjun <daydaygyj@gmail.com>
2017-07-27 16:40:08 +0800

    add some code

--- design-pattern/watcher.js
+++ design-pattern/watcher.js
@@ -2,7 +2,7 @@
  * @Author: Ge.Yuanjun 
  * @Date: 2017-07-27 10:41:53 
  * @Last Modified by: Ge.Yuanjun
- * @Last Modified time: 2017-07-27 14:26:55
+ * @Last Modified time: 2017-07-27 16:36:51
  * @desc: 观察者模式的简单实现
  * 利用全局对象Event来实现，订阅者不需要知道消息来自哪个发送者
  * 发布者也不知道消息会推送给给哪些订阅者
@@ -75,3 +75,143 @@ setTimeout(() => {
 setTimeout(() => {
   Event.trigger('squareMeter88', 2000010)
 }, 6000)
+
+/*************************创建命名空间***************************/
+
+/**
+ * 确保
+ */
+var Event = (() => {
+  var global = this,
+    Event,
+    _default = 'default';
+  
+  Event = function() {
+    var _listen,
+      _trigger,
+      _remove,
+      _slice = Array.prototype.slice,
+      _shift = Array.prototype.shift,
+      _unshift = Array.prototype.unshift,
+      namespaceCache = {},
+      _create,
+      find,
+      each = function(ary, fn) {
+        var ret
+        for (var i = 0, l = ary.length; i < l; i++ ) {
+          var n = ary[i]
+          ret = fn.call(n, i, n)
+        }
+        return ret
+      }
+
+    _listen = function(key, fn, cache) {
+      if (!cache[key]) {
+        cache[key] = []
+      }
+      cache[key].push(fn)
+    }
+
+    _remove = function(key, cache, fn) {
+      if (cache[key]) {
+        if (fn) {
+          for (var i = cache[key].length; i>= 0; i--) {
+            if (cache[key][i] === fn) {
+              cache[key].splice(i, 1)
+            }
+          }
+        } else {
+          cache[key] = []
+        }
+      }
+    }
+
+    _trigger = function() {
+      var cache = _shift.call(arguments),
+        key = _shift.call(arguments),
+        args = arguments,
+        _self = this,
+        ret,
+        stack = cache[key]
+
+      if (!stack || !stack.length) {
+        return
+      }
+
+      return each(stack, function() {
+        return this.apply(_self, args)
+      })
+    }
+
+    _create = function(namespace) {
+      var namespace = namespace || _default
+      var cache = {},
+        offlineStack = [], // 离线事件
+        ret = {
+          listen: function(key, fn, last) {
+            _listen(key, fn, cache)
+            if (offlineStack === null) {
+              return
+            }
+            if (last === 'last') {
+              offlineStack.length && offlineStack.pop()
+            } else {
+              each(offlineStack, function() {
+                this()
+              })
+            }
+            offlineStack = null
+          },
+          one: function(key, fn, last) {
+            _remove(key, cache)
+            this.listen(key, fn, last)
+          },
+          remove: function(key, fn) {
+            _remove(key, cache, fn)
+          },
+          trigger: function() {
+            var fn,
+              args,
+              _self = this;
+            
+              _unshift.call(arguments, cache)
+              args = arguments
+              fn = function() {
+                return _trigger.apply(_self, args)
+              }
+
+              if (offlineStack) {
+                return offlineStack.push(fn)
+              }
+              return fn()
+          }
+        }
+
+      return namespace 
+        ? (namespaceCache[namespace] 
+          ? namespaceCache[namespace]
+          : namespaceCache[namespace] = ret)
+        : ret
+    }
+    return {
+      create: _create,
+      one: function(key, fn, last) {
+        var event = this.create()
+        event.one(key, fn, last)
+      },
+      remove: function(key, fn) {
+        var event = this.create();
+        event.remove(key, fn)
+      },
+      listen: function(key, fn, last) {
+        var event = this.create()
+        event.listen(key, fn, last)
+      },
+      trigger: function() {
+        var event = this.create()
+        event.trigger.apply(this, arguments)
+      }
+    }
+  }()
+  return Event
+})()
