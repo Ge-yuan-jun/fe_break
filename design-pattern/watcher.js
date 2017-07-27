758c969f83c3cc5bf867398436856e9734843270
GeYuanjun <daydaygyj@gmail.com>
2017-07-27 14:27:03 +0800

    refactor

--- design-pattern/watcher.js
+++ design-pattern/watcher.js
@@ -2,33 +2,39 @@
  * @Author: Ge.Yuanjun 
  * @Date: 2017-07-27 10:41:53 
  * @Last Modified by: Ge.Yuanjun
- * @Last Modified time: 2017-07-27 11:39:05
+ * @Last Modified time: 2017-07-27 14:26:55
  * @desc: 观察者模式的简单实现
+ * 利用全局对象Event来实现，订阅者不需要知道消息来自哪个发送者
+ * 发布者也不知道消息会推送给给哪些订阅者
  */
 
-const event = {
-  clientList: [],
-  listen(key, fn) {
-    if (!this.clientList[key]) {
-      this.clientList[key] = []
+const Event = (() => {
+  let clientList = [],
+    listen,
+    trigger,
+    remove;
+
+  listen = (key, fn) => {
+    if (!clientList[key]) {
+      clientList[key] = []
     }
-    this.clientList[key].push(fn)
-  },
-  trigger() {
-    const key = Array.prototype.shift.call(arguments),
-      fns = this.clientList[key]
+    clientList[key].push(fn)
+  }
+
+  trigger = (key, ...args) => {
+    const fns = clientList[key]
 
     if (!fns || fns.length === 0) { // 如果没有绑定对应的消息
       return false
     }
 
     for (let i = 0, fn; fn = fns[i++];) {
-      fn.apply(this, arguments)
+      fn.call(this, ...args)
     }
-  },
+  }
   // 取消订阅的事件
-  remove(key, fn) {
-    let fns = this.clientList[key]
+  remove = (key, fn) => {
+    let fns = clientList[key]
     if (!fns) { // 如果key对应的消息没有被人订阅，则直接返回
       return false
     }
@@ -43,33 +49,29 @@ const event = {
       }
     }
   }
-}
 
-const installEvent = (obj) => {
-  for (let i in event) {
-    obj[i] = event[i]
+  return {
+    listen,
+    trigger,
+    remove
   }
-}
+})()
 
 // example
-
-const salesOffices = []
-installEvent(salesOffices)
-
-salesOffices.listen('squareMeter88', fn1 = (price) => {
+Event.listen('squareMeter88', fn1 = (price) => {
   console.log(`价格=${price}`)
 })
 
-salesOffices.listen('squareMeter100', (price) => {
+Event.listen('squareMeter100', (price) => {
   console.log(`价格=${price}`)  
 })
 
-salesOffices.trigger('squareMeter88', 2000000)
+Event.trigger('squareMeter88', 2000000)
 setTimeout(() => {
-  salesOffices.trigger('squareMeter100', 3000000)
-  salesOffices.remove('squareMeter88', fn1)
+  Event.trigger('squareMeter100', 3000000)
+  Event.remove('squareMeter88', fn1)
 }, 5000)
 
 setTimeout(() => {
-  salesOffices.trigger('squareMeter88', 2000010)
+  Event.trigger('squareMeter88', 2000010)
 }, 6000)
