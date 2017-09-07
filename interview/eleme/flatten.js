0f28d3a5b4e639866a256fec840e952bfb9e1d54
GeYuanjun <daydaygyj@gmail.com>
2017-09-07 19:03:17 +0800

    add params

--- interview/eleme/flatten.js
+++ interview/eleme/flatten.js
@@ -8,7 +8,7 @@
 
 const a = [1, [2,3], [3,4,[5,6]], 7,[8,9]]
 
-const flatten = () => {
+const flatten = (arr) => {
 
 } 
 
--- interview/eleme/split.js
+++ interview/eleme/split.js
@@ -9,7 +9,7 @@
 var a = [1,2,3,4,5,6,7,8,9]
 var group = [[], [], [], []]
 
-var split = () => {
+var split = (arr) => {
 
 }
 
