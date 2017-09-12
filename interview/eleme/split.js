f84304403e1a95d3fc48d6b63cde3b6920ccd73d
GeYuanjun <daydaygyj@gmail.com>
2017-09-12 12:40:48 +0800

    better code

--- interview/eleme/split.js
+++ interview/eleme/split.js
@@ -9,6 +9,7 @@
 var a = [1,2,3,4,5,6,7,8,9]
 var group = [[], [], [], []]
 
+// 先计算需要分成几部分，每一个项里面是多少的元素
 var split = (arr) => {
   let n = Math.floor(arr.length / group.length) >= 1 ? 
     Math.floor(arr.length / group.length) :
@@ -25,7 +26,7 @@ var split = (arr) => {
     const ri = Math.floor(Math.random() * arr.length)
     t.push(arr[ri])
     arr.splice(ri, 1)
-    
+
     if (t.length == n) {
       result.push(t)
       t = []
@@ -36,4 +37,17 @@ var split = (arr) => {
   
 }
 
-console.log(split(a))
+// 先乱序，之后再直接塞到group里面
+
+var putton = (arr, group) => {
+  arr.sort(() => Math.random() - 0.5)
+
+  for (let i = 0; i < a.length; i++) {
+    let groupId = i % group.length
+    group[groupId].push(arr[i])
+  }
+
+  return group
+}
+
+console.log(putton(a, group))
