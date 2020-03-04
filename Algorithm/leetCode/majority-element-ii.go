72204f9f542ce6552d70de1420ad1950423474fa
Ge-yuan-jun <daydaygyj@gmail.com>
2020-03-04 13:20:50 +0800

    fix: 修复error

--- Algorithm/leetCode/majority-element-ii.go
+++ Algorithm/leetCode/majority-element-ii.go
@@ -1,19 +1,21 @@
 import "fmt"
+
 func majorityElement(nums []int) []int {
     // 创建返回值
     var res = make([]int, 0)
     if nums == nil || len(nums) == 0 {
         return res
     }
-    // 初始化两个候选人以及他们的计数票
+
+    // 初始化两个候选人 candidate，以及他们的计数票
     cand1 := nums[0]
     count1 := 0
     cand2 := nums[0]
     count2 := 0
 
-    // 摩尔投票法
+    //摩尔投票法
     // 配对阶段
-    for _, num := range nums{
+    for _, num := range nums {
         // 投票
         if cand1 == num {
             count1++
@@ -29,11 +31,12 @@ func majorityElement(nums []int) []int {
             count1++
             continue
         }
-        if cand2 == 0 {
+        if count2 == 0 {
             cand2 = num
             count2++
             continue
         }
+
         count1--
         count2--
     }
@@ -42,17 +45,15 @@ func majorityElement(nums []int) []int {
     count2 = 0
     for _, num := range nums {
         if cand1 == num {
-            count1 ++
-        }
-        if cand2 == num {
-            count2 ++
+            count1++
+        } else if cand2 == num {
+            count2++
         }
     }
 
     if count1 > len(nums)/3 {
         res = append(res, cand1)
     }
-
     if count2 > len(nums)/3 {
         res = append(res, cand2)
     }
