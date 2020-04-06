747245b9d8be7907d25701f9ae4305c4b3ad24cc
Ge-yuan-jun <daydaygyj@gmail.com>
2020-04-07 01:40:14 +0800

    feat: add 骚版本

--- Algorithm/large-number.js
+++ Algorithm/large-number.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Ge.Yuanjun 
+ * @Date: 2020-04-07 01:21:44 
+ * @Last Modified by: Ge.Yuanjun
+ * @Last Modified time: 2020-04-07 01:26:11
+ */
+
+// 简洁版
 export default function add(a, b) {
   let i = a.length - 1
   let j = b.length -1
@@ -36,4 +44,22 @@ export default function add(a, b) {
   }
 
   return ret
+}
+
+/**
+ * 骚操作版本
+ * @param {*} a 
+ * @param {*} b 
+ */
+export function largeNumSum (a, b) {
+  let c = 0
+  let res = ''
+  a = a.split()
+  b = b.split()
+  
+  while (a.length || b.length || c) {
+    c += ~~a.pop() + ~~b.pop() // boolean 型变量c会存在类型转换（true -> 1, false -> 0）
+    res += c % 10
+    c = c > 9 // c 在此处重置未 boolean 型变量 
+  }
 }
