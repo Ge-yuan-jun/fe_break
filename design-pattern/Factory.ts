ecf6ed54414fcfe4be9e76744f7a417ff3008980
geyuanjun <geyuanjun@bilibili.com>
2021-10-13 20:00:33 +0800

    feat: add concreteFactory

--- design-pattern/Factory.ts
+++ design-pattern/Factory.ts
@@ -95,3 +95,114 @@ console.log('')
 
 console.log('App: ConcreteCreator2 发射')
 clientCode(new ConcreteCreator2())
+
+/************* 抽象工厂模式实现代码 *************/
+/**
+ * 抽象工厂接口声明了一组能返回不同抽象产品的方法。这些产品属于同一个系列
+ * 且在高层主题或概念上具有相关性。同系列的产品通常能相互搭配使用。系列产
+ * 品可有多个变体，但不同变体的产品不能搭配使用
+ */
+interface AbstractFactory {
+  createProductA(): AbstractProductA
+  createProductB(): AbstractProductB
+}
+
+/**
+ * 具体工厂可生成属于同一变体的系列产品。工厂会确保其创建的产品能相互搭配
+ * 使用。具体工厂方法签名会返回一个抽象产品，但在方法内部则会对具体产品进
+ * 行实例化
+ */
+class ConcreteFactory1 implements AbstractFactory {
+  public createProductA(): AbstractProductA {
+    return new ConcreteProductA1()
+  }
+
+  public createProductB(): AbstractProductB {
+    return new ConcreteProductB1()
+  }
+}
+
+/* 每个具体工厂中都会包含一个相应的产品变体 */
+class ConcreteFactory2 implements AbstractFactory {
+  public createProductA(): AbstractProductA {
+    return new ConcreteProductA1()
+  }
+
+  public createProductB(): AbstractProductB {
+    return new ConcreteProductB2()
+  }
+}
+
+// 系列产品中的特定产品必须有一个基础接口。所有产品变体都必须实现这个接口
+interface AbstractProductA {
+  usefulFunctionA(): string
+}
+
+// 具体产品由相应的具体工厂创建
+class ConcreteProductA1 implements AbstractProductA {
+  // 根据 Windows 样式渲染按钮
+  public usefulFunctionA(): string {
+    return '产品A1'
+  }
+}
+
+class ConcreteProductA2 implements AbstractProductA {
+  // 根据 macOS 样式渲染按钮
+  public usefulFunctionA(): string {
+    return '产品A2'
+  }
+}
+
+/**
+ * 这是另一个产品的基础接口。所有产品都可以互动，但是只有相同具体变体的产
+ * 品之间才能够正确地进行交互
+ */
+interface AbstractProductB {
+  // 产品B 可以有自己的方法
+  usefulFunctionB(): string
+
+  // 跟产品 A 可以联动
+  anotherUsefulFunctionB(collaborator: AbstractProductA): string
+}
+
+class ConcreteProductB1 implements AbstractProductB {
+  public usefulFunctionB(): string {
+    return '产品B1'
+  }
+
+  public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+    const result = collaborator.usefulFunctionA()
+    return `B1 与 ${result} 联动`
+  }
+}
+
+class ConcreteProductB2 implements AbstractProductB {
+  public usefulFunctionB(): string {
+    return 'The result of the product B2.'
+  }
+
+  public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+    const result = collaborator.usefulFunctionA()
+    return `The result of the B2 collaborating with the (${result})`
+  }
+}
+
+/**
+ * 客户端代码仅通过抽象类型（GUIFactory、Button 和 Checkbox）使用工厂
+ * 和产品。这让你无需修改任何工厂或产品子类就能将其传递给客户端代码
+ */
+function clientCodeB(factory: AbstractFactory) {
+  const productA = factory.createProductA()
+  const productB = factory.createProductB()
+
+  console.log(productB.usefulFunctionB())
+  console.log(productB.anotherUsefulFunctionB(productA))
+}
+
+/**
+ * 程序会根据当前配置或环境设定选择工厂类型，并在运行时创建工厂（通常在初
+ * 始化阶段）
+ */
+console.log('Client: 测试第一个工厂类型')
+clientCodeB(new ConcreteFactory1())
+clientCodeB(new ConcreteFactory2())
